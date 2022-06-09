c3f0598f4c17aa091f477d20b87f908bd85dd5cf
JasmiRani <106468684+JasmiRani@users.noreply.github.com>
2022-06-09 10:36:39 +0530

    Add files via upload

---
+++ no 4 divisible by 5 and 11 or not.py
@@ -0,0 +1,8 @@
+#.Write a python program to check whether a number is divisible by 5 and 11 or
+#not.
+
+num=int(input("enter number"))
+if num%5==0 and num%11==0:
+    print("divisivale by 5 and 7")
+else:
+    print("not divisivale by 5 and 7") 
